ba88d5186309221da6148fcf0075bbd252765d15
brusmelGil <brusmel@outlook.es>
2024-03-13 15:44:42 +0100

    unfinished login

---
+++ src/components/login/LoginComponet.vue
@@ -0,0 +1,60 @@
+<script setup>
+
+
+</script>
+
+<template>
+
+    <div class="container">
+        <img src="../../assets/imageLogin/11x12.png" alt="" style="display: block; margin: 0 auto; width: 40%;">
+        <div class="card-header text-center no-border-bottom">
+                <h2 class="text-white">Iniciar Sesión</h2>
+            </div>
+
+<div class="row justify-content-center">
+    <div class="col-md-6">
+        <div class="card">
+
+
+            <div class="card-body">
+                <form @submit.prevent="handleSubmit">
+                    <div class="mb-3">
+                        <input type="text" class="form-control" id="username" v-model="username"
+                            placeholder="Usuario">
+                    </div>
+                    <div class="mb-3">
+                        <input type="password" class="form-control" id="password" v-model="password"
+                            placeholder="Contraseña">
+                    </div>
+                    <div class="d-flex justify-content-center"><button type="submit" class="btn btn-primary">ENTRAR</button></div>
+                    
+                </form>
+                <div class="mt-3">
+                    <p class="text-white">¿No estás registrado? <a href="#" @click="register" class="text-white">Regístrate aquí</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+
+</template>
+
+<style scoped>
+.card {
+    background-color: transparent;
+    border: none;
+
+
+    .text-white {
+        color:white;
+        font-family: Arial, Helvetica, sans-serif;
+    }
+
+
+    a {
+        text-decoration: none;
+    }
+
+}
+</style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+import LoginComponet from '@/components/login/LoginComponet.vue';
+</script>
+
+<template>
+<main>
+    <LoginComponet/>
+</main>
+
+</template>
+
+<style>
+
+main{
+    background-image: url(../../src/assets/imageLogin/04.jpg);
+    background-repeat: no-repeat;
+    background-size: cover;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 16px;
+    height: 100vh;
+    width: 100%;
+    background-position: center;
+    
+
+}
+
+</style>
