236691bc9010f878b5ab6acdda3834a374c390f8
Matilde García Suárez <matigarcia0817@gmail.com>
2024-04-02 21:47:41 +0200

    creando responsive al body

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -148,5 +148,14 @@ font-size: 28px;
 .information td {
 padding: 15px;
 }
-
+@media screen and (max-width: 768px) {
+    .container {
+      flex-direction: column; 
+    }
+    .information .more-information {
+      margin-left: 0; 
+      margin-top: 15px; 
+      text-align: center; 
+    }
+  }
 </style>
