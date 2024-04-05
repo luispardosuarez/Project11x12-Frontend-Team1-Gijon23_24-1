bd4d38173829ec984ca1d487270f6736a9799994
brusmelGil <brusmel@outlook.es>
2024-04-05 10:24:11 +0200

    g

--- src/stores/user.js
+++ src/stores/user.js
@@ -2,10 +2,12 @@ export default class User  {
     id 
     username
     password
+    email
 
-    constructor (id, username, password){
+    constructor (id, username, password, email){
         this.id = id;
         this.username = username;
         this.password = password;
+        this.email = email;
     }
 }
