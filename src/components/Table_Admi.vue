a38244c880d3d9e0a455586a50cbcc8cb5b02922
Stiven Diaz <steven_el22@hotmail.com>
2024-04-12 10:35:14 +0200

    changed in hover the select

--- src/components/Table_Admi.vue
+++ src/components/Table_Admi.vue
@@ -7,22 +7,37 @@
         <h4>Becados</h4>
         <table>
             <tr>
-                <td >DNI: 55555555Y</td>
+                <td>DNI: 55555555Y</td>
                 <td>
-                    <img src="../assets/icons/see.svg" alt=""> 
+                    <img src="../assets/icons/see.svg" alt="">
                     <img src="../assets/icons/edit.svg" alt="">
                     <img src="../assets/icons/delete.svg" alt="">
                 </td>
             </tr>
-          
         </table>
-
+        <nav aria-label="Page navigation example">
+            <ul class="pagination">
+                <li class="page-item">
+                    <a class="page-link" href="#" aria-label="Previous">
+                        <span aria-hidden="true">&laquo;</span>
+                    </a>
+                </li>
+                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                <li class="page-item">
+                    <a class="page-link" href="#" aria-label="Next">
+                        <span aria-hidden="true">&raquo;</span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
     </div>
 </template>
 
 <style scoped lang="scss">
 .table_becados {
-    height: 75vh;
+    height: 85vh;
     background-color: $gray-form;
     padding: 2% 2%;
 
@@ -38,22 +53,46 @@
         font-size: 0.8em;
         gap: 6px;
 
-        tr{
-            height:5vh;
+        tr {
+            height: 5vh;
 
             td {
                 padding: 0.3em;
                 width: 1%;
                 border: 1px solid $red;
                 background-color: white;
-        //         text-align: right;
+                text-align: right;
             }
 
             img {
                 width: 30px;
-                padding-left: 3px;
+                padding-right: 3px;
             }
         }
+    }
+
+    .pagination {
+        display: flex;
+        border-radius: 4px;
+        justify-content: center;
+    }
+
+    .pagination>li {
+        display: inline-flex;
+    }
+
+    .pagination>li>a,
+    .pagination>li>span {
+        position: relative;
+        line-height: 18px;
+        color: $gray;
+    }
+
+    .pagination>li.active>a,
+    .pagination>li.active>span {
+        background-color: $red;
+        border-color: $red;
+        color: $red;
     }
 }
 </style>
