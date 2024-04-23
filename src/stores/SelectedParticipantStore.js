1037a8dc63b912b0edfd9324a4743a628e399869
Alicia González Castro <aliglezcastro@gmail.com>
2024-04-23 11:26:06 +0200

    modificado el store de participantes para poder añadir, borrar, y ver los participants seleccionados

--- src/stores/SelectedParticipantStore.js
+++ src/stores/SelectedParticipantStore.js
@@ -1,17 +1,23 @@
 import { defineStore } from "pinia";
 
 export const useSelectedParticipantStore = defineStore({
-    id: 'participant',
+    id: 'selectedParticipant',
     state:() => ({
         selectedParticipants: [],
     }),
 
     actions: {
-        addSelectedParticipant(participant) {
-          this.selectedParticipants.push(participant);
-        },
-        clearSelectedParticipants() {
-          this.selectedParticipants = [];
-        }
-     }
-    });
+      addSelectedParticipant(participant) {
+        this.selectedParticipants.push(participant);
+      },
+      removeSelectedParticipant(participant) {
+        this.selectedParticipants = this.selectedParticipants.filter(p => p !== participant);
+      },
+      clearSelectedParticipants() {
+        this.selectedParticipants = [];
+      },
+      getSelectedParticipants() {
+        return this.selectedParticipants;
+      },
+    },
+  });
