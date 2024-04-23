ca5d876900baaf1f01bbdef5eb61ad29b667144b
Alicia González Castro <aliglezcastro@gmail.com>
2024-04-23 20:30:02 +0200

    creado store para users sin exito de obtener id, tb creado endpoint para users

--- src/stores/user.js
+++ src/stores/user.js
@@ -1,13 +1,47 @@
-export default class User  {
-    id 
-    username
-    password
+
+import axios from 'axios';
+import { defineStore } from 'pinia';
+
+export const useUserStore = defineStore('user', {
+ state: () => ({
+    id: null,
+    username: '',
+    password: '',
+    roles: [],
+ }),
+ actions: {
+
+   async fetchUser(id) {
+      const uri = import.meta.env.VITE_API_ENDPOINT_USERS
+      try {
+         const response = await axios.get(`${uri}/${id}`);
+         this.setUser(response.data);
+      } catch (error) {
+         console.error('Error fetching user:', error);
+      }
+   },
+    setUser(user) {
+      this.id = user.id;
+      this.username = user.username;
+      this.password = user.password;
+      this.roles = user.roles;
+    },
+ },
+});
+
+
+
+
+// export default class User  {
+//     id 
+//     username
+//     password
     
 
-    constructor (id, username, password){
-        this.id = id;
-        this.username = username;
-        this.password = password;
+//     constructor (id, username, password){
+//         this.id = id;
+//         this.username = username;
+//         this.password = password;
         
-    }
-} 
+//     }
+// } 
