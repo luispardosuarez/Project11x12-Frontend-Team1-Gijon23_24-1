e093b5998ffc26190464bf5178fbfa0ecee22f37
Stella Maris González Robert <stegonyrob@yahoo.es>
2024-04-23 16:31:49 +0200

    fixed css home

--- src/components/home/CardDescription.vue
+++ src/components/home/CardDescription.vue
@@ -2,75 +2,71 @@
 import { useProjectStore } from '../../stores/dataHomeStore';
 import ContainerModal from "./ContainerModal.vue";
 const projectStore = useProjectStore();
+
+
 </script>
-
 <template>
-  <div class="description">
-    <div class="description-card">
-      <h1>Descripción</h1>
-      <div class="content-wrapper">
-        <p>{{ projectStore.description }}</p>
-        <img src="../../assets/img/11x12.jpg" alt="imagen logo 11x12" class="logoHome" />
+  <div class="description ">
+    <div class="description-card d-flex justify-content-between align-items-center">
+      <div>
+        <h1>Descripción</h1>
+        <p> {{ projectStore.description }}</p>
       </div>
+      <img src="../../assets/img/11x12.jpg" alt="imagen logo 11x12" class=" logoHome" />
     </div>
-    <ContainerModal/>
+   <ContainerModal/>
   </div>
-</template>
 
+
+</template>
 <style lang="scss" scoped>
 .description {
   display: flex;
   flex-direction: column;
   align-items: center;
-}
-
-.description-card {
-  position: relative; 
-  background-color: #fff;
-  margin-bottom: 5rem;
-  padding: 3rem;
-  border: none;
-  text-align: justify;
-  font-size: 1.3rem;
-
-  h1 {
-    color: $red;
-    margin-left: 52%;
-    @media (max-width: 750px) {
-      margin-left: 40%;
-        
-      }
-      @media (max-width: 360px) {
-      margin-left: 30%;
-        
-      }
-  }
-
-  .content-wrapper {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+ 
+  .description-card {
+    position: relative; 
+    background-color: #fff;
+    margin-bottom: 5rem;
+    padding: 3rem;
+    border: none;
+    text-align: justify;
+    font-size: 1.3rem;
+    margin-left: 1rem;
+   
+    h1 {
+      color: $red;
+      margin-left: 52%;
+    }
 
     p {
+      justify-content: center;
       padding: 2rem;
     }
-
-    .logoHome {
-      width: 15%;
-
-      @media (max-width: 1050px) {
-        width: 25%;
-      }
-
-      @media (max-width: 980px) {
-        width: 35%;
-      }
-
-      @media (max-width: 750px) {
-        width: 55%;
-        
-      }
-    }
   }
+
+  
+  
+
+  .logoHome {
+    width: 15%;
+    display: flex;
+
+    @media (max-width: 1050px) {
+      margin-top: 1rem; 
+      width: 25%;
+      display: flex;
+
+ 
+    }
+    @media (max-width: 980px) {
+      margin-top: 1rem; 
+      width: 35%;
+    }
+   
+  }
+
 }
 </style>
+
